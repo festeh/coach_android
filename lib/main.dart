7f363706354f56359f89826ec256e2321f6973c0
Dmitrii Lipin <dimich3d@ya.ru>
2025-03-24 22:24:31 +0100

    init bg work

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:flutter_background_service_ios/flutter_background_service_ios.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initBgService();
   runApp(const MyApp());
 }
 
+Future<void> initBgService() async {
+  final service = FlutterBackgroundService();
+
+  service.configure(
+    androidConfiguration: AndroidConfiguration(
+      autoStart: true,
+      isForegroundMode: true,
+      onStart: onStart,
+      initialNotificationTitle: 'Coach',
+      initialNotificationContent: 'Focus',
+    ),
+    iosConfiguration: IosConfiguration(autoStart: true),
+  );
+}
+
+@pragma('vm:entry-point')
+Future<bool> onStart(ServiceInstance service) async {
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+  return true;
+}
